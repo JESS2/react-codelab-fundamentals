5eeafac0e8dd2ee1f337d45c41c94ce75dc3eebf
jessie <je_ss2@naver.com>
2018-02-14 21:56:52 +0900

    전화번호부 검색기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
+import Contact from './Contact'; //Contact를 렌더링하기 위해 import
 
 class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 0
-        }
-    }
-
-    render(){
-
-        return (
-            <div>
-                <h1>Hello React Skeleton!!!!!!</h1>
-                <h2>{this.state.value}</h2>
-                <button onClick={() => { this.setState({value: this.state.value + 1}) }}>CLICK ME</button>
-            </div>
-        );
+    render() {
+      return (
+        <Contact/> //Contact를 렌더링
+      );
     }
 }
 
